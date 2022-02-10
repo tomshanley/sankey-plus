50ae399ce653d25593b5c7c0d21e0edce6cbd826
Tom Shanley <tom.shanley@gmail.com>
2022-02-10 13:24:37 +1300

    Fixed linkAngle function

--- src/linkAttributes.js
+++ src/linkAttributes.js
@@ -79,3 +79,11 @@ export function linkPerpendicularYToLinkSource(longerLink, shorterLink) {
 
     return yPerpendicular;
 }
+
+// Return the angle between a straight line between the source and target of the link, and the vertical plane of the node
+function linkAngle(link) {
+    var adjacent = Math.abs(link.y1 - link.y0);
+    var opposite = Math.abs(link.target.x0 - link.source.x1);
+
+    return Math.atan(opposite / adjacent);
+}
